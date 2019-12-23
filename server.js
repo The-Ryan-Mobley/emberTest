e1d93e1216721e8dfdff31fd9143367b5e80aba0
Ryan Mobley <ryan.mobley94@gmail.com>
2019-12-23 12:52:15 -0600

    can run backend with ember serve

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+//const routes = require("./routes");
+const PORT = process.env.PORT || 1337;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hookScrape");
+mongoose.set('useFindAndModify', false);
+app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
